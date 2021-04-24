de536301d1cccf8fe95fff0001773a8cafa16356
Twivex <twivex42@gmail.com>
2021-04-24 21:09:34 +0200

    add bootstrap (JS, must haves)

---
+++ dist/src/js/_bootstrap.js
@@ -0,0 +1,9 @@
+// Option 2
+//
+// Import just what we need
+
+// If you're importing tooltips or popovers, be sure to include our Popper.js dependency
+// import "../../node_modules/popper.js/dist/popper.min.js";
+
+import "../../node_modules/bootstrap/js/dist/util.js";
+import "../../node_modules/bootstrap/js/dist/modal.js";
