f3d6fd9922827c997c3ca7678ab4fb68096aa0f6
Twivex <twivex42@gmail.com>
2021-04-26 16:29:07 +0200

    bootstrap navbar

--- dist/site/snippets/nav.php
+++ dist/site/snippets/nav.php
@@ -1,19 +1,31 @@
-<nav id="menu" class="menu">
-  <?php
-    foreach ($site->children()->listed() as $item) {
-      // skip secured pages, if not logged in
-      if ($item->secured()->toBool() === true && !$kirby->user()) continue;
-      echo $item->title()->link();
-    }
-  ?>
-  <?php if ($kirby->user()): ?>
-    <li>
-      <a href="<?= url('logout') ?>">Logout</a>
-    </li>
-  <?php else: ?>
-    <?php
-      $login = $pages->findBy('uid', 'login');
-      echo $login->title()->link();
-    ?>
-  <?php endif ?>
+<nav id="menu" class="navbar navbar-expand-sm sticky-top navbar-dark bg-dark">
+  <div class="container">
+    <a class="navbar-brand" href="/">Home</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <?php
+          foreach ($site->children()->listed() as $item) :
+          // skip secured pages, if not logged in
+          if ($item->secured()->toBool() === true && !$kirby->user()) continue;
+        ?>
+          <li class="nav-item">
+            <a class="nav-link<?php e($item->isOpen(), ' active') ?>" href="<?= $item->url() ?>"><?= $item->title()->html() ?></a>
+          </li>
+        <?php endforeach ?>
+      </ul>
+      <ul class="navbar-nav justify-content-end">
+        <li class="nav-item">
+          <?php if ($kirby->user()): ?>
+            <a class="nav-link" href="<?= url('logout') ?>">Logout</a>
+          <?php else: ?>
+            <?php $login = $pages->findBy('uid', 'login'); ?>
+            <a class="nav-link<?php e($login->isOpen(), ' active') ?>" href="<?= $login->url() ?>"><?= $login->title()->html() ?></a>
+          <?php endif ?>
+        </li>
+      </ul>
+    </div>
+  </div>
 </nav>
