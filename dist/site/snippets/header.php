e4f7c2f563006f584683b0172745074a641cc691
Twivex <twivex42@gmail.com>
2022-05-10 22:22:16 +0200

    feat(header): add rel links for font files

--- dist/site/snippets/header.php
+++ dist/site/snippets/header.php
@@ -1,6 +1,8 @@
 <html>
   <head>
     <link rel="stylesheet" href="resources/css/style.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Questrial">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sacramento">
   </head>
   <body>
     <header>
