29e282c052154d405257ee2b4192c33198a19760
Twivex <twivex42@gmail.com>
2022-06-02 22:30:09 +0200

    feat: import complete js from bootstrap

--- dist/src/js/_bootstrap.js
+++ dist/src/js/_bootstrap.js
@@ -1,12 +1,1 @@
-// Option 2
-//
-// Import just what we need
-
-// If you're importing tooltips or popovers, be sure to include our Popper.js dependency
-// import "../../node_modules/popper.js/dist/popper.min.js";
-
-import "bootstrap/js/dist/util.js";
-import "bootstrap/js/dist/modal.js";
-import "bootstrap/js/dist/collapse.js";
-import "bootstrap/js/dist/button.js";
-import "bootstrap/js/dist/dropdown.js";
+import * as bootstrap from "bootstrap";
