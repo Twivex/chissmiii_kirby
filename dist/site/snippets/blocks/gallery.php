69beef5bddaca6a8b192c2648a9b9ddcb107fe8c
Twivex <twivex42@gmail.com>
2022-06-08 18:41:40 +0200

    fix(gallery): correct sizes issue

--- dist/site/snippets/blocks/gallery.php
+++ dist/site/snippets/blocks/gallery.php
@@ -13,7 +13,7 @@
     <?php foreach ($block->images()->toFiles() as $image): ?>
       <div class="carousel-item <?=$isFirst ? 'active' : '' ?>">
        <div class="d-flex justify-content-center align-items-center m-0" style="height: calc(100vh - 56px)">
-         <img src="<?= $image->url() ?>" srcset="<?= $image->srcset() ?>" class="d-block w-100" alt="<?= $image->title() ?>">
+         <img src="<?= $image->url() ?>" srcset="<?=$image->srcset()?>" class="d-block mw-100 mh-100" alt="<?=$image->title()?>">
         </div>
       </div>
       <?php $isFirst = false; ?>
@@ -21,11 +21,11 @@
   </div>
 
   <?php if ($showControls): ?>
-    <button class="carousel-control-prev w-7 bg-no-gradient" type="button" data-bs-target="#carousel-<?=$block->id()?>"  data-bs-slide="prev">
+    <button class="carousel-control-prev w-7 bg-no-gradient" type="button" data-bs-target="#carousel-<?=$block->id()?>" data-bs-slide="prev">
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
       <span class="visually-hidden">Previous</span>
     </button>
-    <button class="carousel-control-next w-7 bg-no-gradient" type="button" data-bs-target="#carousel-<?=$block->id()?>"  data-bs-slide="next">
+    <button class="carousel-control-next w-7 bg-no-gradient" type="button" data-bs-target="#carousel-<?=$block->id()?>" data-bs-slide="next">
       <span class="carousel-control-next-icon" aria-hidden="true"></span>
       <span class="visually-hidden">Next</span>
     </button>
