ac0f5fd4ca810381b5df258d649d62f817faf120
Twivex <twivex42@gmail.com>
2022-07-09 21:03:19 +0200

    fix(meta): add missing meta tag to display correctly on mobile

--- dist/site/snippets/globals/header.php
+++ dist/site/snippets/globals/header.php
@@ -4,8 +4,10 @@
 
 <html>
   <head>
+    <meta charset="utf-8">
     <title><?=$page->title()?> | <?=$site->title()?></title>
     <meta name="title=" content="<?=$page->title()?> | <?=$site->title()?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="/resources/css/style.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Questrial">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sacramento">
