7a3a11c2954bbcc17a637a3bb24f7b2214daf612
Twivex <twivex42@gmail.com>
2022-07-11 22:44:55 +0200

    feat(snippets): add placeholder to polaroid if no image given

--- dist/site/snippets/molecules/polaroid.php
+++ dist/site/snippets/molecules/polaroid.php
@@ -1,3 +1,7 @@
-<div class="polaroid" polaroid-caption="<?=$image->title()?>">
-  <img src="<?=$image->url()?>" srcset="<?=$image->srcset()?>" alt="<?=$image->title()?>">
+<div class="polaroid" polaroid-caption="<?=$image ? $image->title() : ''?>">
+  <?php if ($image): ?>
+    <img src="<?=$image->url()?>" srcset="<?=$image->srcset()?>" alt="<?=$image->title()?>">
+  <?php else: ?>
+    <img src="https://via.placeholder.com/200" alt="Placeholder 200x200">
+  <?php endif; ?>
 </div>
