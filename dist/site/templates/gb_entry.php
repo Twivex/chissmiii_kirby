2a0aaaf8afb8abf364eae360016e9fe06da79840
Twivex <twivex42@gmail.com>
2022-07-11 23:00:53 +0200

    fix(template): readd gb entry (needed to get template name)

---
+++ dist/site/templates/gb_entry.php
@@ -0,0 +1,9 @@
+<?php snippet('globals/header') ?>
+
+<main id="main" class="container my-sm-4 px-sm-4 py-4">
+  <section class="py-4">
+    <?= snippet('components/' . $page->template()->name(), ['data' => $page]) ?>
+  </section>
+</main>
+
+<?php snippet('globals/footer') ?>
