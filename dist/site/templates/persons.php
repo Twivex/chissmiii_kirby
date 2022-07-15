98bfb1082b4cdbdc6cbf4c8eed952a91bc5d6e8f
Twivex <twivex42@gmail.com>
2022-07-16 00:50:35 +0200

    chore(persons): remove unnecessary markup

--- dist/site/templates/persons.php
+++ dist/site/templates/persons.php
@@ -1,11 +1,7 @@
 <?php snippet('globals/header') ?>
 
 <main id="main" class="container my-sm-4 px-sm-4 py-4">
-  <section>
-    <div class="row text-center">
-      <?php snippet('components/' . $page->template()->name(), [ 'data' => $page ]); ?>
-    </div>
-  </section>
+  <?php snippet('components/' . $page->template()->name(), [ 'data' => $page ]); ?>
 </main>
 
 <?php snippet('globals/footer') ?>
