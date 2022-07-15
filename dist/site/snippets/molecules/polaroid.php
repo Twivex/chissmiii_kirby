0b4a13fb7da88cd72f31910d35d9a3560439fb1f
Twivex <twivex42@gmail.com>
2022-07-16 00:24:09 +0200

    feat(molecules): add optional size variable to polaroid

--- dist/site/snippets/molecules/polaroid.php
+++ dist/site/snippets/molecules/polaroid.php
@@ -1,10 +1,11 @@
 <?php
   $caption = $caption ?? ($image ? $image->title() : '');
+  $size = $size ?? 200;
 ?>
-<div class="polaroid" polaroid-caption="<?=$caption?>">
+<div class="polaroid polaroid-<?=$size?>" polaroid-caption="<?=$caption?>">
   <?php if ($image): ?>
     <img src="<?=$image->url()?>" srcset="<?=$image->srcset()?>" alt="<?=$image->title()?>">
   <?php else: ?>
-    <img src="https://via.placeholder.com/200" alt="Placeholder 200x200">
+    <img src="https://via.placeholder.com/<?=$size?>" alt="Placeholder <?=$size?>x<?=$size?>">
   <?php endif; ?>
 </div>
