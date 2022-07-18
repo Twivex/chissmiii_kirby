0e08e92ad94c035ecf4c60900f9dc8d2eb0e69a9
Twivex <twivex42@gmail.com>
2022-07-18 08:08:50 +0200

    feat(polaroid): responsive images by adding srcset + optional alt var

--- dist/site/snippets/molecules/polaroid.php
+++ dist/site/snippets/molecules/polaroid.php
@@ -4,7 +4,14 @@
 ?>
 <div class="polaroid polaroid-<?=$size?>" polaroid-caption="<?=$caption?>">
   <?php if ($image): ?>
-    <img src="<?=$image->url()?>" srcset="<?=$image->srcset()?>" alt="<?=$image->title()?>">
+    <img
+      src="<?=$image->url()?>"
+      srcset="<?=$image->srcset([
+        "1x" => [ 'width' => $size],
+        "2x" => [ 'width' => $size * 2],
+        "3x" => [ 'width' => $size * 3]
+      ])?>"
+      alt="<?=$alt ?? $caption?>">
   <?php else: ?>
     <img src="https://via.placeholder.com/<?=$size?>" alt="Placeholder <?=$size?>x<?=$size?>">
   <?php endif; ?>
