cd25c9cfc6cde530fa1e31b2d7da5c3a6c2ee8d2
Twivex <twivex42@gmail.com>
2022-07-29 04:13:07 +0200

    feat(templates+snippets): update guestbook due to changes in blueprint + controller

--- dist/site/snippets/pages/guestbook._.php
+++ dist/site/snippets/pages/guestbook._.php
@@ -1,45 +1,112 @@
 <?php
-  $show_title = $show_title ?? $data->show_title()->toBool();
-  $titleDirecectionClass = $data->title_text_direction()->directionClass();
+  $isTopLevel = $data->parents()->count() === 0;
+  $headingLevel = $isTopLevel ? 'h1' : 'h2';
+  $show_title = $isTopLevel ? true : $data->show_title()->toBool();
+  $titleDirectionClass = $data->title_text_direction()->directionClass();
+  $colsCount = $data->cols_count()->optionKey();
+  $colsCountClass = $colsCount ? "col-lg-$colsCount" : '';
 ?>
 
 <?php if ($show_title): ?>
-  <h2 class="<?=$titleDirectionClass?>"><?=$data->title()?></h2>
+  <div class="row justify-content-center">
+    <div class="col-12 <?=$colsCountClass?>">
+      <<?=$headingLevel?> class="<?=$titleDirectionClass?>"><?=$data->title()?></<?=$headingLevel?>>
+    </div>
+  </div>
 <?php endif; ?>
 
-<section>
-  <form data-guestbook="form" action="<?=$data->url()?>" method="POST" class="needs-validation" novalidate>
-    <fieldset>
-      <div class="mb-3">
-        <label for="name" class="form-label">Name*</label>
-        <input type="text" class="form-control" id="name" aria-describedby="nameHelp"
-          value="<?=$form_data['name'] ?? null?>" required>
-        <div id="nameHelp" class="form-text">Der Name wird für alle sichtbar sein.</div>
-        <div class="invalid-feedback">Bitte gib einen Namen ein.</div>
-      </div>
-      <div class="mb-3">
-        <label for="message" class="form-label">Nachricht*</label>
-        <textarea class="form-control" id="message" aria-describedby="messageHelp"
-          required><?= $form_data['message'] ?? null ?></textarea>
-        <div id="messageHelp" class="form-text">Der Text wird für alle sichtbar sein.</div>
-        <div class="invalid-feedback">Bitte gib eine Nachricht ein.</div>
-      </div>
-      <div class="visually-hidden">
-        <input type="website" name="website" id="website"
-          value="<?= isset($form_data['website']) ? esc($form_data['website']) : null ?>" />
-      </div>
-      <button type="submit" class="btn btn-primary">Abschicken</button>
-      <div class="mt-3">
-        <small class="text-muted">* Pflichtfeld</small>
-      </div>
-    </fieldset>
-  </form>
+<section class="mt-4">
+  <div class="row justify-content-center">
+    <div class="col-12 <?=$colsCountClass?>">
+
+      <form
+        data-entry-collector
+        data-entry-collector-mode="append"
+        data-entry-collector-for="guestbook-list"
+        action="<?=$data->url()?>" method="POST"
+        class="needs-validation"
+        novalidate
+      >
+        <fieldset>
+
+          <div class="row mb-1">
+            <div class="col">
+              <div class="form-floating mb-4">
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  class="form-control"
+                  placeholder="Name*"
+                  aria-describedby="nameValidation"
+                  value="<?=$form_data['name'] ?? null?>"
+                  required
+                >
+                <label for="name" class="form-label">Name*</label>
+                <div id="nameValidation" class="invalid-feedback position-absolute">Bitte gib einen Namen ein.</div>
+                <div id="nameHelp" class="form-text float-end">Der Name wird für alle sichtbar sein.</div>
+              </div>
+            </div>
+          </div>
+
+          <div class="row mb-1">
+            <div class="col">
+              <div class="form-floating mb-4">
+                <textarea
+                  id="message"
+                  name="message"
+                  class="form-control"
+                  placeholder="Nachricht*"
+                  aria-describedby="messageValidation"
+                  style="height: 150px"
+                  required
+                ><?= $form_data['message'] ?? null ?></textarea>
+                <label for="message" class="form-label">Nachricht*</label>
+                <div id="messageValidation" class="invalid-feedback position-absolute">Bitte gib eine Nachricht ein.</div>
+                <div id="messageHelp" class="form-text float-end">Der Text wird für alle sichtbar sein.</div>
+              </div>
+            </div>
+          </div>
+
+          <div class="visually-hidden">
+            <input type="website" name="website" id="website"
+              value="<?= isset($form_data['website']) ? esc($form_data['website']) : null ?>" />
+          </div>
+
+          <div class="d-flex flex-row align-items-baseline justify-content-between mt-2">
+            <button type="submit" class="btn btn-primary">Abschicken</button>
+            <p class="text-muted mb-0">* Pflichtfeld</p>
+          </div>
+
+        </fieldset>
+      </form>
+    </div>
+  </div>
 </section>
 
-<hr>
+<div class="row justify-content-center">
+  <div class="col-12 <?=$colsCountClass?>">
+    <hr>
+  </div>
+</div>
 
-<section class="mt-4 mb-n3" data-guestbook="entries">
-  <?php foreach ($data->children()->sortBy('created_at', 'desc') as $entry) {
-    snippet('pages/guestbook.entry', [ 'data' => $entry ]);
-  } ?>
+<section id="guestbook-list" class="mt-2">
+  <?php if (!$data->hasChildren()): ?>
+
+    <div class="row justify-content-center">
+      <div class="col-12 <?=$colsCountClass?>">
+        <p class="text-muted">Noch keine Einträge vorhanden.</p>
+      </div>
+    </div>
+
+  <?php else: ?>
+
+    <?php foreach ($data->children()->sortBy('created_at', 'desc') as $entry) {
+      snippet('pages/guestbook.entry', [
+        'data' => $entry,
+        'colsCount' => $colsCount
+      ]);
+    } ?>
+
+  <?php endif; ?>
 </section>
--- dist/site/snippets/pages/guestbook.entry.php
+++ dist/site/snippets/pages/guestbook.entry.php
@@ -1,5 +1,8 @@
-<div class="row">
-  <div class="col-12">
+<?php
+  $colsCountClass = $colsCount ? "col-lg-$colsCount" : '';
+?>
+<div class="row justify-content-center">
+  <div class="col-12 <?=$colsCountClass?>">
     <h5><?=$data->name()?> – <small class="text-muted"><?=$data->created_at()->toDate('d.m.y H:i')?></small></h5>
     <p class="text"><?=$data->message()?></p>
   </div>
