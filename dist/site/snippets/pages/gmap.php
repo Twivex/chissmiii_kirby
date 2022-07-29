c1ed372f72c18743fa1e6376b8730f713f4235d0
Twivex <twivex42@gmail.com>
2022-07-29 04:13:40 +0200

    feat(snippets): update gmap due to changes in blueprint

--- dist/site/snippets/pages/gmap.php
+++ dist/site/snippets/pages/gmap.php
@@ -1,14 +1,23 @@
 <?php
-  $show_title = $show_title ?? $data->show_title()->toBool();
-  $titleDirecectionClass = $data->title_text_direction()->directionClass();
-  $col_nums = substr($data->col_nums(), 0, strpos($data->col_nums(), '/'));
+  $isTopLevel = $data->parents()->count() === 0;
+  $headingLevel = $isTopLevel ? 'h1' : 'h2';
+  $show_title = $isTopLevel ? true : $data->show_title()->toBool();
+  $titleDirectionClass = $data->title_text_direction()->directionClass();
+  $colsCount = $data->cols_count()->optionKey();
+  $colsCountClass = $colsCount ? "col-lg-$colsCount" : '';
 ?>
-<section class="py-4">
+
+<?php if ($show_title): ?>
   <div class="row justify-content-center">
-    <div class="col-12 col-lg-<?=$col_nums?>">
-      <?php if ($show_title): ?>
-        <h2 class="mb-3 <?=$titleDirecectionClass?>"><?=$data->title()?></h2>
-      <?php endif; ?>
+    <div class="col-12 <?=$colsCountClass?>">
+      <<?=$headingLevel?> class="<?=$titleDirectionClass?>"><?=$data->title()?></<?=$headingLevel?>>
+    </div>
+  </div>
+<?php endif; ?>
+
+<section class="py-4">
+  <div class="row justify-content-center mb-4">
+    <div class="col-12 <?=$colsCountClass?>">
       <iframe
         style="border:0"
         loading="lazy"
