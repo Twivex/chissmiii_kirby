36a852ff0e89b895d84ab6d030c9659c057d62f6
Twivex <twivex42@gmail.com>
2022-07-29 04:14:17 +0200

    feat(js): add alternative listeners to bs-dismiss

--- dist/src/js/app.js
+++ dist/src/js/app.js
@@ -1,3 +1,20 @@
-import Guestbook from "./modules/guestbook";
+import EntryCollector from "./modules/entry-collector";
 
-Guestbook.init();
+EntryCollector.init();
+
+document.querySelectorAll("[data-hide]").forEach((el) => {
+  el.addEventListener("click", (e) => {
+    const targetEl = el.closest(`.${el.dataset.hide}`);
+    targetEl.classList.remove("show");
+  });
+});
+
+document.querySelectorAll("[data-visually-hide]").forEach((el) => {
+  el.addEventListener("click", (e) => {
+    const targetEl = el.closest(`.${el.dataset.visuallyHide}`);
+    targetEl.classList.remove("show");
+    setTimeout(() => {
+      targetEl.classList.add("visually-hidden");
+    }, 150);
+  });
+});
