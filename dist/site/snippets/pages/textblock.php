79857cbbc3be9fe3d75a6b5876cecb93d8cb6902
Twivex <twivex42@gmail.com>
2022-08-01 16:41:36 +0200

    feat: use field blueprint for cols_count in textblock

--- dist/site/snippets/pages/textblock.php
+++ dist/site/snippets/pages/textblock.php
@@ -1,10 +1,11 @@
 <?php
   $textDirectionClass = $data->text_direction()->directionClass();
-  $col_num = $data->col_num()->optionKey();
+  $colsCount = $data->cols_count()->optionKey();
+  $colsCountClass = $colsCount ? "col-lg-$colsCount" : '';
 ?>
 <section class="py-4">
   <div class="row justify-content-center">
-    <div class="col-12 col-sm-<?=$col_num?> <?=$textDirectionClass?>">
+    <div class="col-12 <?= $colsCountClass ?> <?= $textDirectionClass ?>">
       <?=kt($data->text())?>
     </div>
   </div>
