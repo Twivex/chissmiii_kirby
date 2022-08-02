0a9f88f0db823a8589118e6360b44f9559af7c56
Twivex <twivex42@gmail.com>
2022-08-02 15:13:52 +0200

    feat(plugins): extend custom plugin by page methods

--- dist/site/plugins/custom/index.php
+++ dist/site/plugins/custom/index.php
@@ -1,6 +1,47 @@
 <?php
-Kirby::plugin('chissmiii/custom-field-methods', [
+Kirby::plugin('chissmiii/custom', [
+
+  'pageMethods' => [
+
+    'isTopLevel' => function () {
+      return $this->parents()->count() === 0;
+    },
+
+    'showTitle' => function () {
+      if ($this->blueprint()->field('show_title') !== null) return $this->show_title()->toBool();
+      if ($this->isTopLevel()) return true;
+      return false;
+    },
+
+    'headingLevel' => function () {
+      $headingLevel = $this->isTopLevel() ? '1' : '2';
+      return $headingLevel;
+    },
+
+    'headingTextDirectionClass' => function () {
+      if ($this->title_text_direction()->isNotEmpty()) {
+        return $this->title_text_direction()->directionClass();
+      }
+      return '';
+    },
+
+    'columns' => function () {
+      if ($this->cols_count()->isNotEmpty()) return $this->cols_count()->optionKey();
+      return null;
+    },
+
+    'columnsClass' => function ($size = 'lg') {
+      if (!empty($cols = $this->columns())) {
+        return "col-$size-$cols";
+      }
+      return '';
+    }
+
+  ],
+
+
   'fieldMethods' => [
+
     'directionClass' => function ($field) {
       $directionClass = '';
       if (strpos($field->key(), 'text_direction') !== false) {
@@ -18,6 +59,7 @@ Kirby::plugin('chissmiii/custom-field-methods', [
       }
       return $directionClass;
     },
+
     'optionKey' => function ($field) {
       $blueprintField = $field->model()->blueprint()->field($field->key());
       if (isset($blueprintField['type']) && $blueprintField['type'] === 'select') {
@@ -29,5 +71,7 @@ Kirby::plugin('chissmiii/custom-field-methods', [
       }
       return $field->value();
     }
+
   ]
+
 ]);
