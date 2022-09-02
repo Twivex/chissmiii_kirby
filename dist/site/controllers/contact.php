c4dc73d40c2055b4d78419fc709e273b75b2cb7d
Twivex <twivex42@gmail.com>
2022-09-02 23:28:37 +0200

    fix(controllers): correct validator for phone number

--- dist/site/controllers/contact.php
+++ dist/site/controllers/contact.php
@@ -19,7 +19,7 @@ return function ($kirby, $page) {
     $rules = [
       'name' => ['required'],
       'email' => ['email'],
-      'phone' => ['phone'],
+      'phone' => ['number'],
       'message' => ['required']
     ];
 
