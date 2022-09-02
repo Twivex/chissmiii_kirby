56b018226b449d4f05e0b992873a24383e1cd690
Twivex <twivex42@gmail.com>
2022-09-02 23:13:00 +0200

    feat(js): add form validation to all forms

--- dist/src/js/modules/entry-collector.js
+++ dist/src/js/modules/entry-collector.js
@@ -5,6 +5,7 @@ export default class EntryCollector {
     const form = new Form(
       `[data-entry-collector-for="${entryCollectorTarget}"]`
     );
+    form.preventDefaultSubmit();
     form.onSubmit((e, form) => {
       this.submitEntry(form);
     });
