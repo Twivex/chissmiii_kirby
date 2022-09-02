6e4c104f96e0b1c8399b335e1573616c4d43e74a
Twivex <twivex42@gmail.com>
2022-09-02 23:10:13 +0200

    feat(snippets): add source type bg image

--- dist/site/snippets/globals/bg-img.php
+++ dist/site/snippets/globals/bg-img.php
@@ -4,13 +4,15 @@
     $bgImg = $bgImages->first();
 ?>
   <picture class="background-img">
-    <source srcset="<?=$bgImg->srcset([
-      '780w' => [ 'width' => 780 ],
-      '1024w' => [ 'width' => 1024 ],
-      '1440w' => [ 'width' => 1440 ],
-      '2400w' => [ 'width' => 2400 ]
-    ])?>"
+    <source
+      type="<?= $bgImg->mime() ?>"
+      srcset="<?= $bgImg->srcset([
+        '780w' => [ 'width' => 780 ],
+        '1024w' => [ 'width' => 1024 ],
+        '1440w' => [ 'width' => 1440 ],
+        '2400w' => [ 'width' => 2400 ]
+      ]) ?>"
     >
-    <img src="<?=$bgImg->url()?>">
+    <img src="<?= $bgImg->url() ?>">
   </picture>
 <?php endif; ?>
