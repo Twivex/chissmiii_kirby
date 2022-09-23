1ce68142c37cb89c1c2f498917b0ed808b0b43f2
Twivex <twivex42@gmail.com>
2022-09-23 22:47:27 +0200

    feat(snippets): improve header

--- dist/site/snippets/globals/header.php
+++ dist/site/snippets/globals/header.php
@@ -20,15 +20,19 @@
   }
 ?>
 
+<!DOCTYPE html>
 <html lang="de">
   <head>
     <meta charset="UTF-8">
     <title><?= $page->title()?> | <?=$site->title() ?></title>
     <meta name="title=" content="<?= $page->title()?> | <?=$site->title() ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <link rel="stylesheet" href="/resources/css/style.css">
+
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sacramento">
+
     <?php if ($appIconExists): ?>
       <link rel="apple-touch-icon" href="<?= $appIcon->url() ?>">
     <?php endif; ?>
@@ -38,6 +42,7 @@
     <?php endif; ?>
     <meta name="apple-mobile-web-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="#fbb8ab">
+
     <?php if ($faviconExists): ?>
       <link rel="apple-touch-icon" sizes="180x180" href="<?= $favicon->resize(180)->url() ?>">
       <link rel="shortcut icon" type="image/png" sizes="32x32" href="<?= $favicon->resize(32)->url() ?>">
