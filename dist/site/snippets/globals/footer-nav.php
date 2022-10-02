ede193f1dafe73eb9578cc16e87e47ea4895b1d0
Twivex <twivex42@gmail.com>
2022-10-02 19:20:02 +0200

    feat(snippets+styles): improve responsive behaviour of footer nav

--- dist/site/snippets/globals/footer-nav.php
+++ dist/site/snippets/globals/footer-nav.php
@@ -3,23 +3,23 @@
   $dprgPage = $pages->find('datenschutzerklarung');
 ?>
 
-<nav id="footer-nav" class="navbar navbar-dark navbar-expand bg-light justify-content-center py-1">
+<nav id="footer-nav" class="navbar navbar-dark navbar-expand-sm bg-light justify-content-center py-1">
   <div class="container-fluid container-lg justify-content-center">
-    <ul class="navbar-nav nav-pipe">
+    <ul class="navbar-nav nav-pipe-sm">
 
-      <li class="nav-item">
+      <li class="nav-item d-flex justify-content-center">
         <a class="nav-link" href="<?= $imprintPage->url() ?>">
           <?= $imprintPage->title()->html() ?>
         </a>
       </li>
 
-      <li class="nav-item">
+      <li class="nav-item d-flex justify-content-center">
         <a class="nav-link" href="<?= $dprgPage->url() ?>">
           <?= $dprgPage->title()->html() ?>
         </a>
       </li>
 
-      <li class="nav-item">
+      <li class="nav-item d-flex justify-content-center">
         <span class="nav-link" tabindex="0" data-cookie-consent="openSettings">
           <?= t('cookie-settings-title') ?>
         </span>
