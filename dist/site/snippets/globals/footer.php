f0780a475ab29a4c28308892d127a28a696f2297
Twivex <twivex42@gmail.com>
2022-10-14 23:26:30 +0200

    feat(snippets): move some gloval snippets from header to footer
    visible elements go first

--- dist/site/snippets/globals/footer.php
+++ dist/site/snippets/globals/footer.php
@@ -4,6 +4,9 @@
       <?php snippet('globals/footer-nav'); ?>
     </footer>
 
+    <?php snippet('globals/bg-img') ?>
+    <?php snippet('globals/cookie-banner') ?>
+
     <script src="/resources/js/app.js"></script>
   </body>
 </html>
