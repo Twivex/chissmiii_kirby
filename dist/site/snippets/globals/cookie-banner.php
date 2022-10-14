8a8aa75883a3b0d9c23baeb744136961945d44d3
Twivex <twivex42@gmail.com>
2022-10-14 23:30:01 +0200

    feat(snippets): improve mobile design of cookie banner

--- dist/site/snippets/globals/cookie-banner.php
+++ dist/site/snippets/globals/cookie-banner.php
@@ -3,24 +3,26 @@
   // $stasticCookieAccepted = isset($_COOKIE['cookie-consent-statistic']) && $_COOKIE['cookie-consent-statistic'] == 1;
 ?>
 
-<div id="cookieConsent" class="container fixed-bottom p-0 px-2 pb-3 d-none" aria-live="polite" aria-atomic="true">
-    <div id="cookieConsentBanner" class="toast bg-dark text-white w-100 mw-100" role="alert" data-autohide="false">
+<div id="cookieConsent" class="container fixed-bottom p-0 px-2 mb-3 d-none" aria-live="polite" aria-atomic="true">
+    <div id="cookieConsentBanner" class="toast bg-secondary text-white w-100 mw-100" role="alert" data-autohide="false">
         <div class="toast-body p-4 d-flex flex-column">
 
             <h4 class="fs-5"><?= t('cookie-title') ?></h4>
             <?= kirbytext(t('cookie-text')) ?>
 
-            <div class="d-flex flex-row justify-content-between">
+            <div class="d-flex flex-wrap gap-3 gap-sm-0">
 
-              <button type="button" class="btn btn-outline-primary" id="cookieConsentOpenSettings">
-                <?= t('cookie-open-settings') ?>
-              </button>
+              <div class="d-grid g-4 g-sm-0 d-sm-block col-12 col-sm-6">
+                <button type="button" class="btn btn-outline-primary" id="cookieConsentOpenSettings">
+                  <?= t('cookie-open-settings') ?>
+                </button>
+              </div>
 
-              <div class="d-flex flex-row gap-sm-3 gap-2">
-                <button type="button" class="btn btn-primary" id="cookieConsentAccept">
+              <div class="grid d-sm-flex justify-content-end gap-3 gap-sm-2 col-12 col-sm-6">
+                <button type="button" class="btn btn-primary g-col-6" id="cookieConsentAccept">
                   <?= t('cookie-accept') ?>
                 </button>
-                <button type="button" class="btn btn-primary" id="cookieConsentDeny">
+                <button type="button" class="btn btn-primary g-col-6" id="cookieConsentDeny">
                   <?= t('cookie-deny') ?>
                 </button>
               </div>
@@ -30,7 +32,7 @@
         </div>
     </div>
 
-    <div id="cookieConsentSettings" class="toast bg-dark text-white w-100 mw-100" role="alert" data-autohide="false">
+    <div id="cookieConsentSettings" class="toast bg-secondary w-100 mw-100" role="alert" data-autohide="false">
         <div class="toast-body p-4">
 
             <h4 class="fs-5"><?= t('cookie-settings-title') ?></h4>
