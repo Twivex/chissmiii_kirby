975dc1e107bdd5360c988f6adaf4242b3c62985d
Twivex <twivex42@gmail.com>
2022-10-15 06:53:21 +0200

    feat(snippets): improve responsive behaviour of nav
    expand on large screens only;
    correct some paddings & margins;

--- dist/site/snippets/globals/nav.php
+++ dist/site/snippets/globals/nav.php
@@ -1,4 +1,4 @@
-<nav id="menu" class="navbar navbar-dark navbar-expand-md sticky-top bg-light pt-sm-3">
+<nav id="menu" class="navbar navbar-dark navbar-expand-lg sticky-top bg-light pt-sm-3">
   <div class="container-fluid container-lg">
     <a class="navbar-brand fs-6 mb-n3 mb-sm-n1 font-decorative" href="/"><?= $site->title() ?></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -6,7 +6,7 @@
     </button>
     <div class="offcanvas offcanvas-end" tabindex="-1" id="navbarContent" aria-labelledby="navbarLabel">
       <div class="offcanvas-header bg-light text-white">
-        <h5 class="offcanvas-title mb-n1 mt-1" id="navbarLabel">Menü</h5>
+        <h5 class="offcanvas-title mb-n1 mt-1 ms-1 ps-2" id="navbarLabel">Menü</h5>
         <button type="button" class="btn-close btn-close-white text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
       </div>
       <div class="offcanvas-body">
@@ -22,7 +22,7 @@
         ?>
           <?php if ($item->intendedTemplate()->name() === 'virtual-page'): ?>
 
-            <li class="nav-item dropdown ms-md-4">
+            <li class="nav-item dropdown ms-1 ms-lg-4">
               <a class="nav-link dropdown-toggle pe-0 ps-2 position-relative <?php e($item->isOpen(), 'active') ?>" href="#" id="navbarDropdown<?= ucfirst($key) ?>" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 <?= $item->title()->html() ?>
               </a>
@@ -32,14 +32,14 @@
                   if (!$kirby->user()) $subNavItems = $subNavItems->listed();
                 ?>
                 <?php foreach($subNavItems as $subItem): ?>
-                  <li><a class="dropdown-item ps-4 <?php e($subItem->isOpen(), 'active') ?>" href="<?= $subItem->url() ?>" <?php e($subItem->isOpen(), 'aria-current="true"') ?>><?= $subItem->title()->html() ?></a></li>
+                  <li><a class="dropdown-item py-2 py-lg-3 ps-4 ps-lg-3 <?php e($subItem->isOpen(), 'active') ?>" href="<?= $subItem->url() ?>" <?php e($subItem->isOpen(), 'aria-current="true"') ?>><?= $subItem->title()->html() ?></a></li>
                 <?php endforeach; ?>
               </ul>
             </li>
 
           <?php else: ?>
 
-            <li class="nav-item ms-md-4">
+            <li class="nav-item ms-1 ms-lg-4">
               <a class="nav-link pe-0 ps-2 position-relative <?php e($item->isOpen(), 'active') ?>" href="<?= $item->url() ?>"><?= $item->title()->html() ?></a>
             </li>
           <?php endif; ?>
