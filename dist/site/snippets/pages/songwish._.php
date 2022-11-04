0e7822e2f336cd6f48b71848485e1a823557a06a
Twivex <twivex42@gmail.com>
2022-11-04 21:54:08 +0100

    fix(snippets): extract songwish list into own snippet for the controller

--- dist/site/snippets/pages/songwish._.php
+++ dist/site/snippets/pages/songwish._.php
@@ -105,42 +105,6 @@
 
 <section id="songwish-list" class="mt-2">
 
-  <div class="row justify-content-center">
-    <div class="col-12 <?= $data->columnsClass() ?>">
-
-      <h3>Bisher eingereichte Liedwünsche</h3>
-
-      <?php if (!$data->hasChildren()): ?>
-
-        <p class="text-muted">Noch keine Liedwünsche vorhanden.</p>
-
-      <?php else: ?>
-
-        <?php
-          $entryGroups = $data->children()
-            ->sortBy('song_artist', 'asc', 'song_title', 'asc')
-            ->group(function($child) {
-              return Str::substr($child->title()->value(), 0, 1);
-            });
-        ?>
-
-        <?php foreach ($entryGroups as $key => $entries): ?>
-
-          <div class="row justify-content-left mt-3">
-
-            <h4><?= strtoupper($key) ?></h4>
-
-            <?php foreach ($entries as $entry) {
-              snippet('pages/songwish.entry', [ 'data' => $entry ]);
-            } ?>
-
-          </div>
-
-        <?php endforeach; ?>
-
-      <?php endif; ?>
-
-    </div>
-  </div>
+  <?= snippet('pages/songwish.list', [ 'data' => $data ]) ?>
 
 </section>
