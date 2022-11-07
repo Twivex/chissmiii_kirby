bb8191cff5e4b2a57056d5ae2c53b2dda7795a14
Twivex <twivex42@gmail.com>
2022-11-07 20:03:32 +0100

    feat(config): enable debug only on localhost

--- dist/site/config/config.php
+++ dist/site/config/config.php
@@ -2,7 +2,7 @@
 return [
   'routes' => require_once 'routes.php',
   'home' => 'home',
-  'debug'  => true,
+  'debug'  => isset($_SERVER['HTTP_X_FORWARDED_PROTO']),
   'url' => isset($_SERVER['HTTP_X_FORWARDED_PROTO']) ? $_SERVER['HTTP_X_FORWARDED_PROTO'] . '://chissmiii.home-webserver.de' : 'localhost:' . $_SERVER['SERVER_PORT'],
   'panel' => [
     'language' => 'de'
