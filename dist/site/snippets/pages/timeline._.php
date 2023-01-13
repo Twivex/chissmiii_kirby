7cf84acfea2eb553a0a582743815ac6e3e986cf1
Twivex <twivex42@gmail.com>
2023-01-13 22:26:45 +0100

    feat(timeline): use material symbols instead of unicode chars

--- dist/site/snippets/pages/timeline._.php
+++ dist/site/snippets/pages/timeline._.php
@@ -7,11 +7,23 @@
     if (!$kirby->user()) {
       $entries = $entries->listed();
     }
+
+    $c = 0;
     foreach ($entries as $entry) {
+      $c++;
+
+      if ($c === 1) {
+        echo '<span class="symbol material-symbols-rounded ms-xlarge">favorite</span>';
+      }
+
       snippet('pages/timeline.entry', [
         'data' => $entry,
         'alternating' => $data->alternating_entries()->toBool()
       ]);
+
+      if ($c === count($entries)) {
+        echo '<span class="symbol material-symbols-rounded ms-xlarge">keyboard_double_arrow_down</span>';
+      }
     }
   ?>
 
