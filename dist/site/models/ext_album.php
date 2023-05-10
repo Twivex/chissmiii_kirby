847de49b91ffe94f817bf6d987ae4d857a64fa42
Twivex <twivex42@gmail.com>
2023-05-10 08:49:10 +0200

    chore(models): add more empty lines for a better reading

--- dist/site/models/ext_album.php
+++ dist/site/models/ext_album.php
@@ -1,25 +1,35 @@
 <?php
 
 class ExtAlbumPage extends Page {
+
   public function getAlbumPath($absolute = false) {
+
     $albumPath = $this->album_path()->replacePathVars();
+
     if ($absolute) {
       $albumPath = $_SERVER['DOCUMENT_ROOT'] . $albumPath;
     }
+
     return $albumPath;
+
   }
 
+
   public function getAlbumFiles($sort = false) {
+
     if ($this->album_path()->isNotEmpty()) {
+
       $albumPath = $this->getAlbumPath();
       $absAlbumPath = $this->getAlbumPath(true);
       $files = [];
 
       // check if directory exists
       if (file_exists($absAlbumPath)) {
+
         $imageExtensions = ['jpg', 'jpeg', 'png', 'gif'];
         $videoExtensions = ['mp4', 'webm', 'mov', 'mpeg', 'mpeg2', 'avi'];
         $allowedExtensions = array_merge($imageExtensions, $videoExtensions);
+
         // get all files in the directory
         $files = array_reduce(
           scandir($absAlbumPath),
@@ -48,10 +58,15 @@ class ExtAlbumPage extends Page {
             return filemtime($a['path']) - filemtime($b['path']);
           });
         }
+
       }
 
       return $files;
+
     }
+
     return [];
+
   }
+
 }
