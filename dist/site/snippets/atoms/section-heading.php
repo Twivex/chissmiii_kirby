a946c9cb52aec5c049b1d045127be9b1ad991a07
Twivex <twivex42@gmail.com>
2023-08-14 17:28:40 +0200

    feat(snippets/section-heading): add back button

--- dist/site/snippets/atoms/section-heading.php
+++ dist/site/snippets/atoms/section-heading.php
@@ -10,10 +10,25 @@
   if (isset($showParentTitle) && $showParentTitle) {
     $title = $data->parent()->title() . ' – ' . $title;
   }
+  $showStickyHeader = !$page->isTopLevel();
 ?>
 
+<?php if ($showStickyHeader): ?>
+  <div class="sticky-header">
+    <div class="sticky-header-body">
+      <?php snippet('atoms/fab', [
+        'iconName' => 'arrow_back',
+        'url' => $data->parent()->url(),
+        'iconSize' => 'large',
+        'additionalClasses' => ['position-absolute', 'btn-nav'],
+        'title' => 'Zurück'
+      ]) ?>
+    </div>
+  </div>
+<?php endif; ?>
+
 <?php if ($data->showTitle()): ?>
-  <div class="row justify-content-center">
+  <div class="section-headline row">
     <div class="col-12 <?= $data->columnsClass() ?>">
       <?= $hTag ?>
         <?= $title ?>
